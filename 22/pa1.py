1501341cf87231bd111fc11fd5381f37e6283847
withzjc <2806623723@qq.com>
2019-03-02 12:28:17 +0800

    111

--- 22/pa1.py
+++ 22/pa1.py
@@ -6,7 +6,7 @@
 import requests
 
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"}
-url_temp = "https://www.zhihu.com/s?wd={}".format("python")
+url_temp = "https://www.zhihu.com"
 
 r = requests.get(url_temp,headers= headers)
 
